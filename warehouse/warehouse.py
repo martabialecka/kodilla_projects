4cf7118b6bd1ff5d733ec2dbdeed7bc26a33b1b7
Marta Białecka <marta.bialecka@outlook.com>
2023-01-15 00:44:57 +0100

    [warehouse] Add items details to warehouse project.

--- warehouse/warehouse.py
+++ warehouse/warehouse.py
@@ -1,5 +1,39 @@
+items = [
+    {'name' :'chocolate',
+    'quantity' : 200,
+    'unit' : 'kg',
+    "unit_price" : 4},
+    {'name' :'sweets',
+    'quantity' : 50,
+    'unit' : 'kg',
+    "unit_price" : 3},
+    {'name' :'lollipop',
+    'quantity' : 400,
+    'unit' : 'kg',
+    "unit_price" : 2}]
+
+def get_items (items):
+    # name:      15 
+    # quantity:  12
+    # unit:       8
+    # unit_price: nvm
+    table =  "Name           Quantity    Unit    Unit Price (PLN)\n"
+    table += "____           ________    ____    __________\n"
+    for item in items:
+        table += \
+            item['name'] + ' ' * (15 - len(item['name'])) + \
+            str(item['quantity']) + ' ' * (12 - len(str(item['quantity']))) + \
+            item['unit'] + ' ' * (8 - len(item['unit'])) + \
+            str(item ['unit_price']) + '\n'
+
+    return table
+
 while True:
-    decision = input ("What would you like to do?")
-    if decision == "exit":
+    decision = input ("What would you like to do? ")
+    if decision == 'show':
+        print (get_items(items))
+    if decision == 'exit':
         print = "Exiting... Bye!"
         break
+
+
