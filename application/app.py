32c46b65e7ef8918c74ac36c0a4c1e2020f5dbf2
Marta Białecka <marta.bialecka@outlook.com>
2023-02-19 01:10:05 +0100

    [application] Some changes.

--- application/app.py
+++ application/app.py
@@ -8,19 +8,29 @@ app.config["SECRET_KEY"] = "nininini"
 User = namedtuple("User", field_names=["username", "password"])
 user = User(username="john", password="black")
 
+from collections import namedtuple
+from flask import Flask, request, render_template
+from forms import EmailPasswordForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "nininini"
+
+User = namedtuple("User", field_names=["email", "password"])
+user = User(email="john@black.com", password="black")
+
 @app.route("/login/", methods=["GET", "POST"])
 def login():
-   form = LoginForm()
-   if request.method == "POST":
-       if (
-           form.username.data == user.username and
-           form.password.data == user.password
-       ):
-           return "You are logged id"
-       else:
-           return "Wrong credentials!!"
-   return render_template("login.html", form=form)
-
+    form = EmailPasswordForm()
+    error = ""
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if form.email.data == user.email and form.password.data == user.password:
+                return "You are logged id"
+            else:
+                return "Wrong credentials!!"
+        else:
+            error = form.errors
+    return render_template("login.html", form=form, error=error)
 
 if __name__ == "__main__":
-   app.run(debug=False)
+    app.run(debug=False)
