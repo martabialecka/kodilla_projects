4a7d0372587e62d32dcc25fe5c2b57d7b8738e13
Marta Białecka <marta.bialecka@outlook.com>
2023-02-24 01:11:33 +0100

    [household_expenses] Decimal instead of float

--- household_expenses/forms.py
+++ household_expenses/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, FloatField, StringField
+from wtforms import BooleanField, DecimalField, StringField
 from wtforms.validators import DataRequired
 
 class ExpensesForm(FlaskForm):
     title = StringField('Nazwa', validators=[DataRequired()])
-    amount = FloatField('Kwota', validators=[DataRequired()])
+    amount = DecimalField('Kwota', validators=[DataRequired()])
     paid = BooleanField ('Zapłacone?')
--- household_expenses/models.py
+++ household_expenses/models.py
@@ -15,7 +15,7 @@ class HHExpenses:
 
     def save_all(self):
         with open('hh_expenses.json', 'w') as f:
-            json.dump(self.data, f)
+            json.dump(self.data, f, default=str)
 
     def all(self):
         return self.data
