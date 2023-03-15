c5549115654739ac6f8077f563d1b40725cd66f6
martabialecka <marta.bialecka@outlook.com>
2023-03-16 00:25:15 +0100

    [stations_db.py] Ready but very slow

--- data_base/stations_db.py
+++ data_base/stations_db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Table, Column, Integer, SmallInteger, String, Date, Float, MetaData, ForeignKey
 from sqlalchemy import create_engine
+from datetime import date
 import csv
 import os
 
@@ -9,7 +10,7 @@ db_file = 'stations.db'
 if os.path.exists(db_file):
     os.remove(db_file)
 
-engine = create_engine(f'sqlite:///{db_file}', echo = True)
+engine = create_engine(f'sqlite:///{db_file}')
 
 meta = MetaData()
 countries = Table(
@@ -25,7 +26,7 @@ states = Table(
 )
 stations = Table(
     'stations', meta,
-    Column('id', SmallInteger, primary_key = True),
+    Column('station', String(11), primary_key = True),
     Column('latitude', Float),
     Column('longitude', Float),
     Column('elevation', Float),
@@ -35,8 +36,8 @@ stations = Table(
 )
 measures = Table(
     'measures', meta,
-    Column('id', Integer, primary_key = True),
-    Column('station', Integer, ForeignKey('stations.id')),
+    Column('id', Integer, primary_key = True, autoincrement = True),
+    Column('station', Integer, ForeignKey('stations.station')),
     Column('date', Date),
     Column('precip', Float),
     Column('tobs', SmallInteger)
@@ -49,5 +50,11 @@ conn.execute(states.insert().values(code = 'HI', country = 'US', name = 'Hawaii'
 
 with open('clean_stations.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
-    print(reader.fieldnames)
+    ins = stations.insert()
+    conn.execute(ins, list(reader))
 
+with open('clean_measure.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    ins = measures.insert()
+    for row in reader:
+        conn.execute(ins.values(station = row['station'], date = date.fromisoformat(row['date']), precip = row['precip'], tobs = row['tobs']))
