f48086a3c7169c652f18a1aa1a72b4e10f61946e
martabialecka <marta.bialecka@outlook.com>
2023-03-15 23:35:16 +0100

    [stations_db.py] Tables ready.

--- data_base/stations_db.py
+++ data_base/stations_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, Date, MetaData, ForeignKey
+from sqlalchemy import Table, Column, Integer, SmallInteger, String, Date, Float, MetaData, ForeignKey
 from sqlalchemy import create_engine
 import csv
 import os
@@ -20,8 +20,26 @@ countries = Table(
 states = Table(
     'states', meta,
     Column('code', String(2), primary_key = True),
-    Column('country_code', String(2), ForeignKey('countries.code')),
+    Column('country', String(2), ForeignKey('countries.code')),
     Column('name', String)
+)
+stations = Table(
+    'stations', meta,
+    Column('id', SmallInteger, primary_key = True),
+    Column('latitude', Float),
+    Column('longitude', Float),
+    Column('elevation', Float),
+    Column('name', String),
+    Column('country', String(2), ForeignKey('countries.code')),
+    Column('state', String(2), ForeignKey('states.code'))
+)
+measures = Table(
+    'measures', meta,
+    Column('id', Integer, primary_key = True),
+    Column('station', Integer, ForeignKey('stations.id')),
+    Column('date', Date),
+    Column('precip', Float),
+    Column('tobs', SmallInteger)
 )
 meta.create_all(engine)
 
